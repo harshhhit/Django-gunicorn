75c3f986c53472d0cf690590fac129ac45655dda
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-13 21:33:32 +0530

    Update ain.sh

--- ain.sh
+++ ain.sh
@@ -9,8 +9,11 @@ After=network.target
 User=ubuntu
 Group=www-data
 WorkingDirectory=/home/ubuntu/
-ExecStart=/home/ubuntu/env/bin/gunicorn 
-         
+ExecStart=/home/ubuntu/env/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          djangot.wsgi:application
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/gunicorn.service
 
@@ -18,14 +21,19 @@ WantedBy=multi-user.target" > /etc/systemd/system/gunicorn.service
 echo "server {
     listen 80;
     server_name 3.110.171.125;
-  
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static/ {
+        root /home/ubuntu;
+    }
     location / {
         include proxy_params;
-        proxy_pass /home/ubuntu/env/bin/gunicorn 
+        proxy_pass http://unix:/run/gunicorn.sock;
     }
 }" > /etc/nginx/sites-available/djangot2
 
+sudo fuser -k 80/tcp
+sudo fuser -k 443/tcp
+
 sudo ln -s /etc/nginx/sites-available/djangot2 /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
-
 sudo systemctl restart nginx
