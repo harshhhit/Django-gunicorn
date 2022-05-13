102939ca0ccf4bc15f06cf77e4f835f464ea3530
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-13 20:59:35 +0530

    Update ain.sh

--- ain.sh
+++ ain.sh
@@ -1,41 +1,13 @@
 #!/bin/bash
 deactivate
-echo "[Unit]
-Description=gunicorn socket
-[Socket]
-ListenStream=/run/gunicorn.sock
-[Install]
-WantedBy=sockets.target" > /etc/systemd/system/gunicorn.socket
-
-echo "[Unit]
-Description=gunicorn daemon
-Requires=gunicorn.socket
-After=network.target
-[Service]
-User=ubuntu
-Group=www-data
-WorkingDirectory=/home/ubuntu/
-ExecStart=/home/ubuntu/env/bin/gunicorn \
-          --access-logfile - \
-          --workers 3 \
-          --bind unix:/run/gunicorn.sock \
-          djangot.wsgi:application
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/gunicorn.service
-
-sudo systemctl start gunicorn.socket
-sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
     server_name 3.110.171.125;
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location /static/ {
-        root /home/ubuntu;
-    }
+   
     location / {
         include proxy_params;
-        proxy_pass http://unix:/run/gunicorn.sock;
+        proxy_pass /home/ubuntu/env/bin/gunicorn ;
     }
 }" > /etc/nginx/sites-available/djangot2
 
