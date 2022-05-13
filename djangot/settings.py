0372ec633889bccd33538a1c2fe6de046b381898
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-13 21:42:46 +0530

    Update settings.py

--- djangot/settings.py
+++ djangot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*lgxapw1o1+h^00@dz+qta!ls=gml(jrx$vi9&%l_nnv*^=yjd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.110.171.125']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
