80fc5969c9627f40ba2bdcd0ce919cb2381323ff
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-13 21:16:01 +0530

    Update ain.sh

--- ain.sh
+++ ain.sh
@@ -4,7 +4,6 @@ deactivate
 
 echo "[Unit]
 Description=gunicorn daemon
-
 After=network.target
 [Service]
 User=ubuntu
@@ -19,16 +18,14 @@ WantedBy=multi-user.target" > /etc/systemd/system/gunicorn.service
 echo "server {
     listen 80;
     server_name 3.110.171.125;
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location /static/ {
-        root /home/ubuntu;
-    }
+  
     location / {
         include proxy_params;
         proxy_pass /home/ubuntu/env/bin/gunicorn 
     }
 }" > /etc/nginx/sites-available/djangot2
 
-
+sudo ln -s /etc/nginx/sites-available/djangot2 /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
 
 sudo systemctl restart nginx
