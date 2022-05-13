b4b5060b151e2e02bae8eb26f90afcbcda1d245b
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-13 20:31:29 +0530

    Update ain.sh

--- ain.sh
+++ ain.sh
@@ -29,7 +29,7 @@ sudo systemctl enable gunicorn.socket
 echo "server {
     listen 80;
     server_name 3.110.171.125;
-    location = /favicon.ico { access_log off; log_not_found off; }
+    #location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
         root /home/ubuntu;
     }
