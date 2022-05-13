3aefb28f9a62415756b8b79186b0eaec02d74b28
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-13 21:02:23 +0530

    Update ain.sh

--- ain.sh
+++ ain.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 deactivate
 
+
+echo "[Unit]
+Description=gunicorn daemon
+
+After=network.target
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/
+ExecStart=/home/ubuntu/env/bin/gunicorn \
+          
+
 echo "server {
     listen 80;
     server_name 3.110.171.125;
-   
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static/ {
+        root /home/ubuntu;
+    }
     location / {
         include proxy_params;
-        proxy_pass /home/ubuntu/env/bin/gunicorn ;
+        proxy_pass http://unix:/run/gunicorn.sock;
     }
 }" > /etc/nginx/sites-available/djangot2
 
