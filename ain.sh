7e428da59283f701aced2630a54a1bab22a700b3
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-05-15 14:37:02 +0530

    Update ain.sh

--- ain.sh
+++ ain.sh
@@ -1,9 +1,15 @@
-
+#!/bin/bash
 deactivate
-
+echo "[Unit]
+Description=gunicorn socket
+[Socket]
+ListenStream=/run/gunicorn.sock
+[Install]
+WantedBy=sockets.target" > /etc/systemd/system/gunicorn.socket
 
 echo "[Unit]
 Description=gunicorn daemon
+Requires=gunicorn.socket
 After=network.target
 [Service]
 User=ubuntu
@@ -12,29 +18,26 @@ WorkingDirectory=/home/ubuntu/
 ExecStart=/home/ubuntu/env/bin/gunicorn \
           --access-logfile - \
           --workers 3 \
-          --bind 0.0.0.0:8000 \
+          --bind unix:/run/gunicorn.sock \
           djangot.wsgi:application
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/gunicorn.service
 
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
-    server_name 15.207.221.133;
+    server_name 3.110.28.202;
     location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
         root /home/ubuntu;
     }
     location / {
         include proxy_params;
-        proxy_pass http://15.207.221.133:8000;
+        proxy_pass http://unix:/run/gunicorn.sock;
     }
 }" > /etc/nginx/sites-available/djangot2
-
-
-
-
-
 
 sudo ln -s /etc/nginx/sites-available/djangot2 /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
